2df8d4c201d353dde82cc1231efc508d86e15198
Hiro1006 <hota1006@gmail.com>
2022-04-04 06:27:16 +0000

    finish-4

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,7 +4,7 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
    end
 
    def destroy
